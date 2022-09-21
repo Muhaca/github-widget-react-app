8f8425d57266d2408638b4e5936cc00ebb486063
Muhaca <muhamadcasdi@gmail.com>
2022-09-21 17:32:20 +0700

    Create Button

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,22 +1,29 @@
-import React, {Suspense} from "react";
-const RemoteApp = React.lazy(() => import("app2/App"));
+import React, { Suspense } from 'react';
+const RemoteApp = React.lazy(() => import('app2/App'));
 
 const App = () => {
   return (
     <div>
-      <div style={{
-        margin:"10px",
-        padding:"10px",
-        textAlign:"center",
-        backgroundColor:"greenyellow"
-      }}>
+      <div
+        style={{
+          margin: '10px',
+          padding: '10px',
+          textAlign: 'center',
+          backgroundColor: 'greenyellow',
+        }}
+      >
         <h1>App1</h1>
+        <button component="a" href="http://localhost:3002/Page1.js">
+          Page app 1{' '}
+        </button>
+        <button>Page app 2 </button>
+        <button>Page app 3 </button>
       </div>
-      <Suspense fallback={"loading..."}>
-        <RemoteApp/>
+      <Suspense fallback={'loading...'}>
+        <RemoteApp />
       </Suspense>
-    </div>)
-}
-
+    </div>
+  );
+};
 
 export default App;
